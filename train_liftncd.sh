64d9bcf74cfb54df247888ac3680bcc511954a41
jingzhengli <maxlijingzheng@163.com>
2023-04-29 14:32:38 +0800

    first commit

--- train_liftncd.sh
+++ train_liftncd.sh
@@ -2,7 +2,7 @@
 python train_flexmatch.py --dataset "ucf101" --batch_size 64 --shots 16
 python train_flexmatch.py --dataset "pets" --batch_size 64 --shots 16
 python train_flexmatch.py --dataset "dtd" --batch_size 64 --shots 16
-Cpython train_flexmatch.py --dataset "eurosat" --batch_size 64 --shots 16
+python train_flexmatch.py --dataset "eurosat" --batch_size 64 --shots 16
 python train_flexmatch.py --dataset "fgvc" --batch_size 64 --shots 16
 python train_flexmatch.py --dataset "caltech101" --batch_size 64 --shots 16
 python train_flexmatch.py --dataset "imagenet" --batch_size 64 --shots 16
